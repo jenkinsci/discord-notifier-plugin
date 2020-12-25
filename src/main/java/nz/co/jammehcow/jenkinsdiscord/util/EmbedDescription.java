a864cd94cf20cbb0f8ade3ae6cd92e0331d2a1d3
Ansis Malins <ansis.malins@gmail.com>
2020-12-25 16:01:09 +0100

    Add option to display changes when using the pipeline step

--- src/main/java/nz/co/jammehcow/jenkinsdiscord/util/EmbedDescription.java
+++ src/main/java/nz/co/jammehcow/jenkinsdiscord/util/EmbedDescription.java
@@ -1,9 +1,10 @@
 package nz.co.jammehcow.jenkinsdiscord.util;
 
-import hudson.model.AbstractBuild;
+import jenkins.scm.RunWithSCM;
 import hudson.model.Run;
 import hudson.scm.ChangeLogSet;
-import hudson.scm.NullSCM;
+import java.util.ArrayList;
+import java.util.Arrays;
 import jenkins.model.JenkinsLocationConfiguration;
 
 import java.util.LinkedList;
@@ -22,7 +23,7 @@ public class EmbedDescription {
     private String finalDescription;
 
     public EmbedDescription(
-            AbstractBuild build,
+            Run build,
             JenkinsLocationConfiguration globalConfig,
             String prefix,
             boolean enableArtifactsList,
@@ -31,12 +32,15 @@ public class EmbedDescription {
         String artifactsURL = globalConfig.getUrl() + build.getUrl() + "artifact/";
         this.prefix = prefix;
 
-        if (!(build.getProject().getScm() instanceof NullSCM) && showChangeset) {
-            this.changesList.add("\n**Changes:**\n");
-            Object[] changes = build.getChangeSet().getItems();
-            if (changes.length == 0) {
+        if (showChangeset) {
+            ArrayList<Object> changes = new ArrayList<>();
+            List<ChangeLogSet> changeSets = ((RunWithSCM)build).getChangeSets();
+            for (ChangeLogSet i : changeSets)
+                changes.add(Arrays.asList(i.getItems()));
+            if (changes.isEmpty()) {
                 this.changesList.add("\n*No changes.*\n");
             } else {
+                this.changesList.add("\n**Changes:**\n");
                 for (Object o : changes) {
                     ChangeLogSet.Entry entry = (ChangeLogSet.Entry) o;
                     String commitID;
@@ -44,8 +48,13 @@ public class EmbedDescription {
                     else if (entry.getCommitId().length() < 6) commitID = entry.getCommitId();
                     else commitID = entry.getCommitId().substring(0, 6);
 
+                    String msg = entry.getMsg().trim();
+                    int nl = msg.indexOf("\n");
+                    if (nl >= 0)
+                        msg = msg.substring(0, nl).trim();
+
                     this.changesList.add(String.format("   - ``%s`` *%s - %s*%n",
-                            commitID, entry.getMsg(), entry.getAuthor().getFullName()));
+                            commitID, EscapeMarkdown(msg), entry.getAuthor().getFullName()));
                 }
             }
         }
@@ -94,5 +103,11 @@ public class EmbedDescription {
     @Override
     public String toString() {
         return this.finalDescription;
+    }
+
+    // https://support.discord.com/hc/en-us/articles/210298617
+    private static String EscapeMarkdown(String text) {
+        return text.replace("\\", "\\\\").replace("*", "\\*").replace("_", "\\_").replace("~", "\\~")
+            .replace("`", "\\`");
     }
 }
