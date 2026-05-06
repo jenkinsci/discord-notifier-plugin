6fdc8a45e722c18f5a87318ca3f6e5ea35c5bf7f
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2026-05-06 08:59:03 +0200

    Migrate tests to JUnit Jupiter
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup
    * Ban JUnit4 imports

--- src/test/java/nz/co/jammehcow/jenkinsdiscord/BasicTest.java
+++ src/test/java/nz/co/jammehcow/jenkinsdiscord/BasicTest.java
@@ -1,26 +1,25 @@
 package nz.co.jammehcow.jenkinsdiscord;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class BasicTest {
+class BasicTest {
+
     @Test
-    public void webhookClassDoesntThrow() {
-        try {
+    void webhookClassDoesntThrow() {
+        assertDoesNotThrow(() -> {
             DiscordWebhook wh = new DiscordWebhook("http://exampl.e");
             wh.setContent("content");
             wh.setDescription("desc");
             wh.setStatus(DiscordWebhook.StatusColor.GREEN);
             wh.send();
-        } catch (Exception e) {
-            fail();
-        }
+        });
     }
 
     @Test
-    public void pipelineDoesntThrow() {
-        try {
+    void pipelineDoesntThrow() {
+        assertDoesNotThrow(() -> {
             DiscordPipelineStep step = new DiscordPipelineStep("http://exampl.e");
             step.setTitle("Test title");
             DiscordPipelineStep.DiscordPipelineStepExecution execution =
@@ -28,8 +27,6 @@ public class BasicTest {
             execution.step = step;
             execution.listener = () -> System.out;
             execution.run();
-        } catch (Exception e) {
-            fail();
-        }
+        });
     }
 }
